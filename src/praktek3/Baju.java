a07dab2cfb830b318408d37ff1bf2f0bc0b32493
thahamh <32389972+thahamh@users.noreply.github.com>
2018-04-10 14:34:51 +0800

    Buat Method cetakInfo()

--- src/praktek3/Baju.java
+++ src/praktek3/Baju.java
@@ -5,4 +5,11 @@ public class Baju {
   String warna;
   String ukuran;
   double harga;
+  
+  void cetakInfo(){
+      System.out.println("Merk \t: "+merk+"\n"+
+              "Warna \t: "+warna+"\n"+
+              "Ukuran \t: "+ukuran+"\n"+
+              "Harga \t:"+harga );
+  }
 }
