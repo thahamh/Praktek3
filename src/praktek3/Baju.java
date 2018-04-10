553e6e1ecb8370473322f3dacd9742e7e6d076f8
thahamh <32389972+thahamh@users.noreply.github.com>
2018-04-10 14:43:34 +0800

    Buat Getter and Setter

--- src/praktek3/Baju.java
+++ src/praktek3/Baju.java
@@ -12,4 +12,38 @@ public class Baju {
               "Ukuran \t: "+ukuran+"\n"+
               "Harga \t:"+harga );
   }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+  
+  
 }
